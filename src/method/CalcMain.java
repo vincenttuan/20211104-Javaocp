c78bb4d4193ce05752be0be3ab50841b360dc8ef
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-09 21:43:33 +0800

    Add files via upload

---
+++ src/method/CalcMain.java
@@ -0,0 +1,22 @@
+package method;
+
+public class CalcMain {
+
+	public static void main(String[] args) {
+		Calc calc = new Calc();
+		int r = calc.add(10, 30);
+		System.out.println(r);
+		
+		double bmiValue = calc.getBMI(170.0, 60.0);
+		System.out.println(bmiValue);
+		
+		int[] x = {100, 90, 80};
+		int sum = calc.getSum(x);
+		System.out.println(sum);
+		
+		calc.welcome("John");
+		
+		calc.welcome();
+	}
+
+}
