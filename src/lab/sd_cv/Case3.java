e5173b99e558725032043306a098af41e254a9de
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-11 20:37:33 +0800

    Add files via upload

--- src/lab/sd_cv/Case3.java
+++ src/lab/sd_cv/Case3.java
@@ -12,8 +12,15 @@ package lab.sd_cv;
 public class Case3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double[] stocks = {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5};  
+		double[] bonds = {6.8, 7.2, 6.8, 7.5, 6.9, 7.9, 7.9, 7.1, 7.2};
+		Calc calc = new Calc();
+		double stock_cv = calc.getCV(stocks); 
+		double bond_cv = calc.getCV(bonds);
+		System.out.println("股票 CV: " + stock_cv);
+		System.out.println("債券 CV: " + bond_cv);
+		String result = stock_cv < bond_cv?"股票":"債券";
+		System.out.println(result + "投資較為穩健");
 	}
 
 }
