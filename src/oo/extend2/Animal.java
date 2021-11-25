8c174bc4106243de112355eb387889c6e1d01060
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-25 20:55:20 +0800

    Add files via upload

---
+++ src/oo/extend2/Animal.java
@@ -0,0 +1,36 @@
+package oo.extend2;
+
+public class Animal {
+	private String name;
+	private int legs;
+	
+	public Animal() {
+		
+	}
+	
+	public Animal(String name, int legs) {
+		this.name = name;
+		this.legs = legs;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLegs() {
+		return legs;
+	}
+
+	public void setLegs(int legs) {
+		this.legs = legs;
+	}
+	
+	public void move() {
+		System.out.println("°Ê");
+	}
+	
+}
---
+++ src/oo/extend2/Main1.java
@@ -0,0 +1,13 @@
+package oo.extend2;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		dog.setName("®ã¤ü");
+		dog.setLegs(4);
+		dog.move();
+		System.out.println(dog);
+	}
+
+}
