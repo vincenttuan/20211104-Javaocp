cdb90a575c802b11be1ea2905203667d600095a4
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-11-25 21:19:19 +0800

    Add files via upload

---
+++ src/oo/extend2/Bird.java
@@ -0,0 +1,10 @@
+package oo.extend2;
+
+public class Bird extends Animal {
+
+	@Override
+	public void move() {
+		System.out.println("­¸");
+	}
+	
+}
---
+++ src/oo/extend2/Ostrich.java
@@ -0,0 +1,10 @@
+package oo.extend2;
+
+public class Ostrich extends Bird {
+
+	@Override
+	public void move() {
+		System.out.println("ил");
+	}
+	
+}
