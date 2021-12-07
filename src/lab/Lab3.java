f14ec03036b480568d59795c41cc22582f4831f7
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-07 21:43:57 +0800

    Add files via upload

---
+++ src/lab/Lab3.java
@@ -0,0 +1,11 @@
+package lab;
+
+public class Lab3 {
+
+	public static void main(String[] args) {
+		int end = new StringBuilder("Happy New Year And Merry Christmas").reverse().toString().indexOf(" ");
+		System.out.println(end);
+		
+	}
+
+}
