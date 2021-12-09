624f6affd51646b81a468587a1755d3fdc4f4f81
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-09 20:08:51 +0800

    Add files via upload

--- src/oo/abstracts2/Main1.java
+++ src/oo/abstracts2/Main1.java
@@ -1,8 +1,16 @@
 package oo.abstracts2;
 
+import java.util.Arrays;
+
 public class Main1 {
 	public static void main(String[] args) {
 		// 所有能夠提供的武器一起發射
+		Weapon w1 = new Tank();
+		Weapon w2 = new Fighter();
+		Weapon[] weapons = {w1, w2};
 		
+		Arrays.stream(weapons).forEach(w -> w.fire());
+		
+		Arrays.stream(weapons).forEach(Weapon::fire);
 	}
 }
