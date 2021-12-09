8e8287589fb72a2a29149ab87d1517a4fe419ead
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-09 21:18:24 +0800

    Add files via upload

---
+++ src/pattern/decorator/Olives.java
@@ -0,0 +1,11 @@
+package pattern.decorator;
+
+public class Olives extends SideDish {
+
+	public Olives(Food food) {
+		super(food);
+		name = "¾ñÆV";
+		price = 10;
+	}
+	
+}
---
+++ src/pattern/decorator/Tomato.java
@@ -0,0 +1,11 @@
+package pattern.decorator;
+
+public class Tomato extends SideDish {
+
+	public Tomato(Food food) {
+		super(food);
+		name = "µf­X";
+		price = 15;
+	}
+	
+}
