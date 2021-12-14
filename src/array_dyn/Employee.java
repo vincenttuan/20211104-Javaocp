5a903d22712c103e3ce1482d8589c4bd29a9fae1
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-14 19:55:48 +0800

    Add files via upload

--- src/array_dyn/Employee.java
+++ src/array_dyn/Employee.java
@@ -1,5 +1,7 @@
 package array_dyn;
 
+import java.util.Objects;
+
 public class Employee {
 	private Integer num; // 員工編號
 	private String name; // 員工姓名
@@ -23,6 +25,23 @@ public class Employee {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, num);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(name, other.name) && Objects.equals(num, other.num);
+	}
+	
 	@Override
 	public String toString() {
 		return "Employee [num=" + num + ", name=" + name + "]";
