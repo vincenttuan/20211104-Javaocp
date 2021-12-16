46654f05e620846f595b631019a404391c9810f5
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-16 20:34:22 +0800

    Add files via upload

---
+++ src/oo/innerclass2/DogImpl.java
@@ -0,0 +1,10 @@
+package oo.innerclass2;
+
+public class DogImpl implements Dog {
+
+	@Override
+	public void skill() {
+		System.out.println("¬Ýªù");
+	}
+	
+}
---
+++ src/oo/innerclass2/DogTest.java
@@ -0,0 +1,11 @@
+package oo.innerclass2;
+
+public class DogTest {
+
+	public static void main(String[] args) {
+		Dog dog = new DogImpl();
+		dog.skill();
+
+	}
+
+}
