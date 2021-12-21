c1ca9b7111a1031f5d2e928563c268d6f6ea3fa3
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-21 19:22:16 +0800

    Add files via upload

---
+++ src/oo/enums/PositionTest.java
@@ -0,0 +1,21 @@
+package oo.enums;
+
+public class PositionTest {
+
+	public static void main(String[] args) {
+		int bonus = 5000;
+		printBonus(Position.協理, bonus);
+		printBonus(Position.專員, bonus);
+		
+		printBonus(6, bonus);
+		printBonus(2, bonus);
+		
+		printBonus(100, bonus);
+	}
+	
+	public static void printBonus(int pos, int bonus) {
+		int real_bonus = pos * bonus;
+		System.out.println(real_bonus);
+	}
+
+}
