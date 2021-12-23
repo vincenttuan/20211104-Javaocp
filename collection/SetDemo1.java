21db49686c5fd29ee210b2e372106727b7faac39
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-12-23 20:55:40 +0800

    Add files via upload

--- collection/SetDemo1.java
+++ collection/SetDemo1.java
@@ -17,19 +17,28 @@ public class SetDemo1 {
 		set.add(80);
 		System.out.println(set);
 		
-		// 走訪元素內容 Iterator
+		// 走訪元素內容 Iterator (一步一步做)
 		Iterator iter = set.iterator();
-		while (iter.hasNext()) {
-			Object object = iter.next();
-			System.out.println(object);
-		}
-		// 再跟 set 要一次 iterator, 才可以尋訪
+		System.out.println(iter.hasNext());
+		System.out.println(iter.next());
+		System.out.println(iter.hasNext());
+		System.out.println(iter.next());
+		System.out.println(iter.hasNext());
+		System.out.println(iter.next());
+		System.out.println(iter.hasNext());
+		System.out.println(iter.next());
+		System.out.println(iter.hasNext());
+		System.out.println(iter.next());
+		System.out.println(iter.hasNext());
+		System.out.println(iter.next());
+		System.out.println(iter.hasNext());
+		System.out.println("---------------");
+		// 走訪元素內容 Iterator (使用 while)
+		// 重新取得走訪器(iter)
 		iter = set.iterator();
 		while (iter.hasNext()) {
-			Object object = iter.next();
-			System.out.println(object);
+			//Object object = iter.next();
+			System.out.println(iter.next());
 		}
-		
-		
 	}
 }
