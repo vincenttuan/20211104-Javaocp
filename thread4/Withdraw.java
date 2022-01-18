35ef9b9f75353d9998c3b3930d652b5424623b63
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-18 21:07:57 +0800

    Add files via upload

---
+++ thread4/Withdraw.java
@@ -0,0 +1,17 @@
+package thread4;
+
+public class Withdraw implements Runnable {
+	private Account account;
+	private int amount;
+	
+	public Withdraw(Account account, int amount) {
+		this.account = account;
+		this.amount = amount;
+	}
+	
+	@Override
+	public void run() {
+		account.withdraw(amount);
+	}
+	
+}
