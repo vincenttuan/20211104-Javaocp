bc5d80796e7c51f758b539bc2b59872c9b25b79d
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-18 21:19:14 +0800

    Add files via upload

---
+++ thread4/ATM.java
@@ -0,0 +1,19 @@
+package thread4;
+
+public class ATM {
+	public static void main(String[] args) {
+		Account account = new Account(10000);
+		
+		Withdraw w1 = new Withdraw(account, 5000);
+		Withdraw w2 = new Withdraw(account, 3000);
+		Withdraw w3 = new Withdraw(account, 4000);
+		
+		Thread t1 = new Thread(w1, "小明");
+		Thread t2 = new Thread(w2, "小華");
+		Thread t3 = new Thread(w3, "小英");
+		
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+}
