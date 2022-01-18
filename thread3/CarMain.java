0042fb694925d073aa71a5cd25a879ccc4aa192e
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-18 19:57:58 +0800

    Add files via upload

--- thread3/CarMain.java
+++ thread3/CarMain.java
@@ -4,8 +4,10 @@ import java.util.concurrent.CyclicBarrier;
 
 public class CarMain {
 	public static void main(String[] args) {
-		int parties = 4;
-		CyclicBarrier cyclicBarrier = new CyclicBarrier(parties);
+		Runnable lunch = () -> System.out.println("吃午餐");
+		
+		int parties = 4; // 2, 1
+		CyclicBarrier cyclicBarrier = new CyclicBarrier(parties, lunch);
 		
 		Thread t1 = new Thread(new Car(cyclicBarrier), "A車");
 		Thread t2 = new Thread(new Car(cyclicBarrier), "B車");
